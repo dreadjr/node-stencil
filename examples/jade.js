8e81c9163974a59815f49ffcb379d79b9beafc29
doug read <qnary@ubuntu.(none)>
2012-10-16 13:04:18 -0700

    simple example and consolidate piggy-back

--- examples/jade.js
+++ examples/jade.js
@@ -4,6 +4,10 @@ var stencil = new stencils.Stencil();
 // try read from extension?
 stencil.render('jade', 'jade.jade', {name: "you!"}, function(err, html) {
   if (err) throw err;
+  console.log(html);
+});
 
+stencil.render('jade', 'jade.jade', {name: "other person!"}, function(err, html) {
+  if (err) throw err;
   console.log(html);
 });
