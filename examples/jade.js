7bb0d4a0e277a3f2f7588c066b7703d66c955b8d
doug read <qnary@ubuntu.(none)>
2012-10-16 13:43:02 -0700

    updated example

--- examples/jade.js
+++ examples/jade.js
@@ -2,12 +2,12 @@ var stencils = require('../lib/stencil');
 var stencil = new stencils.Stencil();
 
 // try read from extension?
-stencil.render('jade', 'jade.jade', {name: "you!"}, function(err, html) {
+stencil.render('jade', 'jade.jade', {email: {name: "you!"}}, function(err, html) {
   if (err) throw err;
   console.log(html);
 });
 
-stencil.render('jade', 'jade.jade', {name: "other person!"}, function(err, html) {
+stencil.render('jade', 'jade.jade', {email: {name: "other person!"}}, function(err, html) {
   if (err) throw err;
   console.log(html);
 });
